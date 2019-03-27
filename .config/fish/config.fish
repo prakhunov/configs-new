6637adb8d04c07eff875bf1f74c351d0772f9346
Peter Rakhunov <prakhunov@gmail.com>
2019-03-27 00:50:14 -0500

    current stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,13 @@ set -gx PATH $PATH ~/.emacs.d/bin/ ~/.npm/bin/
 set -gx XDG_DATA_DIRS /usr/local/share:/usr/share
 set -gx EDITOR /usr/bin/vim
 set -gx BROWSER "/usr/bin/qutebrowser %s"
+
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
+set -U dangerous_nogreeting
+set dangerous_colors night
