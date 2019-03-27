ab51174abedb148b552d3556e7af986957e0ef22
Peter Rakhunov <prakhunov@gmail.com>
2019-03-27 01:19:31 -0500

    new polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,8 +8,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 for i in $(polybar -m | awk -F: '{print $1}'); do 
-  MONITOR=$i polybar bar1 -c ~/.config/polybar/config &
-  MONITOR=$i polybar bar2 -c ~/.config/polybar/config &
+  MONITOR=$i polybar --reload mainbar-i3 &
+#  MONITOR=$i polybar bar2 -c ~/.config/polybar/config &
 done
 
 echo "Bars launched..."
