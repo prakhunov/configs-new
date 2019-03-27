e76c81d2a308e50830ddf9235f7abf5505dc247a
Peter Rakhunov <prakhunov@gmail.com>
2019-03-27 12:36:22 -0500

    new compton stuff

--- .screenlayout/compton.sh
+++ .screenlayout/compton.sh
@@ -10,6 +10,7 @@
 
 #compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45  --paint-on-overlay --unredir-if-possible  --backend glx --glx-no-stencil --glx-no-rebind-pixmap --vsync opengl-mswc
 #compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45  --backend glx --vsync opengl-mswc --sw-opti
-compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45  --backend xrender --unredir-if-possible
+#compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45  --backend xrender --unredir-if-possible
+compton -b --config ~/.screenlayout/compton.conf
 #compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45  --backend glx --glx-swap-method=buffer-age
 #compton -c -r8 -l-12 -t-8  -b  -G  -f -D30 -I0.45 -O0.45 --vsync opengl 
