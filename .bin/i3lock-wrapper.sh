8cdbdeca8d7cda6e535ec62e907c52cc4c95f080
Peter Rakhunov <prakhunov@gmail.com>
2019-03-28 16:51:15 -0500

    changed the blur in the lock screen to be better

--- .bin/i3lock-wrapper.sh
+++ .bin/i3lock-wrapper.sh
@@ -4,6 +4,6 @@ file1=$(mktemp --tmpdir i3lock-wrapper-XXXXXXXXXX.png)
 file2=$(mktemp --tmpdir i3lock-wrapper-XXXXXXXXXX.png)
 
 scrot -d0 "$file1"
-convert "$file1" -blur 0x3 "$file2"
+convert "$file1" -blur 0x8 "$file2"
 i3lock -i "$file2" -n
 rm "$file1" "$file2"
